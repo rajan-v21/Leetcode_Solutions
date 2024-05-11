f7d2bdf27d6d2a3d5aa6f07046769dfa10af9b45
Rajan Vishwakarma <rajanvishwakarma218@gmail.com>
2024-05-11 20:01:41 +0530

    Easy - Time: 2ms (97.59%), Space: 44.13mb (98.43%)

--- Java/0001-two-sum.java
+++ Java/0001-two-sum.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.lang.*;
-class Solution {
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> mp = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
@@ -14,14 +14,14 @@ class Solution {
 
     public static void main(String[] args) {
         // Create an instance of the Solution class
-        Solution solution = new Solution();
+        TwoSum twoSum = new TwoSum();
 
         // Example input
         int[] nums = {2, 7, 11, 15};
         int target = 9;
 
         // Call the twoSum method and print the result
-        int[] result = solution.twoSum(nums, target);
+        int[] result = twoSum.twoSum(nums, target);
         System.out.println("Indices: " + Arrays.toString(result));
     }
 }
