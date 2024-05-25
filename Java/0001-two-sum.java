64ae810c0fa457df3a073e3ba0baac508b278178
Rajan Vishwakarma <rajanvishwakarma218@gmail.com>
2024-05-25 13:11:50 +0530

    Easy - Time: 2ms (97.67%), Space: 44.13mb (98.48%)

---
+++ Java/0001-two-sum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(mp.containsKey(complement))
+                return new int[] {mp.get(complement), i};
+            mp.put(nums[i], i);
+        }
+        return new int[]{};
+    } 
+
+    public static void main(String[] args) {
+        // Create an instance of the Solution class
+        TwoSum twoSum = new TwoSum();
+
+        // Example input
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        // Call the twoSum method and print the result
+        int[] result = twoSum.twoSum(nums, target);
+        System.out.println("Indices: " + Arrays.toString(result));
+    }
+}
